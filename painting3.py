d609dadf2d3a67b99c6066d75b28fb8233ca4a93
wiwi0527 <wiwi20000527@gamil.com>
2024-09-23 10:54:38 +0800

    增加演算法，畫圖程式碼，以及各benchmark的實驗結果檔案。

---
+++ painting3.py
@@ -0,0 +1,30 @@
+import pickle
+import matplotlib.pyplot as plt
+
+# Load the deviations and reuse distance data
+
+with open("perf.data_PmuTime_PhyAddrs.txt_devs.bin", 'rb') as devs_file:
+    deviations = pickle.load(devs_file)
+
+
+
+
+# Create a hist plot
+plt.figure(figsize=(100, 50),dpi= 100)
+#plt.hist( deviations, color='blue', label='Deviations', bins= 250 )
+
+plt.scatter(range(len(deviations)), deviations, color='blue', label='Reuse Distance', s=0.1)
+
+with open("reuse_distance.bin", 'rb') as reuse_distance_file:
+    reuse_distances = pickle.load(reuse_distance_file)
+
+
+plt.scatter(range(len(reuse_distances)), reuse_distances, color='red', label='Reuse Distance', s=0.1)
+
+#plt.scatter(range(len(reuse_distances)), reuse_distances, color='red', label='Reuse Distance', s=10)
+plt.title('Deviations and Reuse Distance Scatter Plot')
+plt.xlabel('Index')
+plt.ylabel('Value')
+plt.legend()
+plt.grid(True)
+plt.show()
